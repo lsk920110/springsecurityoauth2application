be082ee8f022709d1f0412e6ab076064064283d3
lsk <lsk@rowem.com>
2022-11-09 12:11:25 +0900

    init
    Controller에서 clientRegistrationRepository가 null로뜸.
    debug에서는 객체에 값들이 존재하나, 변수에 저장이 안되고 null로 되고 있는 중

---
+++ src/main/java/io/security/springsecurityoauth2application/IndexController.java
@@ -0,0 +1,40 @@
+package io.security.springsecurityoauth2application;
+
+import com.nimbusds.oauth2.sdk.client.ClientRegistrationRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.oauth2.client.CommonOAuth2Provider;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.ClientRegistrations;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+
+@RestController
+public class IndexController {
+
+    @Autowired
+    private
+    ClientRegistrationRepository clientRegistrationRepository;
+
+
+    @GetMapping("/")
+    public String index() {
+
+        ClientRegistration clientRegistration = this.clientRegistrationRepository.findByRegistrationId("keyclock");
+
+        String clientId = clientRegistration.getClientId();
+        System.out.println("clientId = " + clientId);
+//
+        String redirectUri = clientRegistration.getRedirectUri();
+        System.out.println("redirectUri = " + redirectUri);
+//
+
+        return "index";
+    }
+
+
+}
+
---
+++ src/main/java/io/security/springsecurityoauth2application/OAuth2ClientConfig.java
@@ -0,0 +1,35 @@
+package io.security.springsecurityoauth2application;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.ClientRegistrations;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
+
+@Configuration
+public class OAuth2ClientConfig {
+
+    @Bean
+    public ClientRegistrationRepository clientRegistrationRepository() {
+        return new InMemoryClientRegistrationRepository(this.keyclockClientRegistration());
+    }
+
+    private ClientRegistration keyclockClientRegistration() {
+
+        ClientRegistration asd = ClientRegistrations
+                .fromIssuerLocation("http://localhost:8080/realms/oauth2")
+                .registrationId("keycloak")
+                .clientId("oauth2-client-app")
+                .clientSecret("XFqpYz4h9n8NabUhl21jaQa8APi2ROri")
+                .redirectUri("http://localhost:8081/login/oauth2/code/keycloak")
+//                .issuerUri("http://localhost:8080/realms/oauth2")
+                .build();
+        System.out.println("ClientId = "+asd.getClientId());
+
+        return asd;
+    }
+
+
+}
